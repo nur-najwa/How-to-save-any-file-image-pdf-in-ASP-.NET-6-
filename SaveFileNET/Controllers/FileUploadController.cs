190a72b78499039f4b2225ea043cbe62d63471a4
Najwa <najwa@iris.com.my>
2022-12-13 15:09:07 +0800

    first commit

---
+++ SaveFileNET/Controllers/FileUploadController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaveFileNET.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        public IActionResult FileUploadPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SaveFile(IFormFile[] FileArray)
+        {
+            if(FileArray != null && FileArray.Count()>0)
+            {
+                foreach(var file in FileArray)
+                {
+                    if (file != null)
+                    {
+                        string FileNmae = file.FileName;
+                        var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/files", FileNmae);
+
+                        var stream = new FileStream(path, FileMode.Create);
+                        file.CopyToAsync(stream);
+
+                        string url = "/files/" + FileNmae;
+                    }
+                }
+            }       
+            return RedirectToAction("FileUploadPage", "FileUpload");
+        }
+    }
+}
